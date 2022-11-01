91bb8699bfd75cba3b8783bdfae1083da35814d0
tommyjamess <meadigun@gmail.com>
2022-11-01 10:41:38 +0100

    update

---
+++ styles/chart.js
@@ -0,0 +1,52 @@
+var xValues = ["Italy",];
+    var yValues = [50, 50];
+    var barColors = [
+    "#700CB3",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+    ];
+
+    new Chart("mysession", {
+    type: "pie",
+    data: {
+        // labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
+    },
+    // options: {
+    //     title: {
+    //     display: true,
+    //     }
+    // }
+});
+
+
+var xValues = [];
+    var yValues = [55, 40];
+    var barColors = [
+    "#700CB3",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+    ];
+
+    new Chart("mysolution", {
+    type: "pie",
+    data: {
+        // labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
+    },
+    // options: {
+    //     title: {
+    //     display: true,
+    //     }
+    // }
+});
