2e164bfa659085f1f69bd328c1ea7b2b49948977
tommyjamess <meadigun@gmail.com>
2022-11-02 16:32:43 +0100

    update

---
+++ styles/comment.js
@@ -0,0 +1,15 @@
+let commentBtn = document.querySelectorAll("#commentBtn")
+let commentPage = document.querySelectorAll(".comment-page")
+
+
+commentBtn.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        if(btn.parentElement.nextElementSibling.classList.contains("openComment")){
+            btn.parentElement.nextElementSibling.classList.remove("openComment")
+            // btn.parentElement.nextElementSibling.style.display = "none"
+        }else{
+            btn.parentElement.nextElementSibling.classList.add("openComment")
+            // btn.parentElement.nextElementSibling.style.display = "block"
+        }
+    })
+})
