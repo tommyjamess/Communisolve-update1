92ed23210ef955e1c6797afe6761afc9692c72ef
tommyjamess <meadigun@gmail.com>
2022-11-03 22:30:08 +0100

    update

--- styles/comment.js
+++ styles/comment.js
@@ -1,17 +1,43 @@
 let commentBtn = document.querySelectorAll("#commentBtn")
 let commentPage = document.querySelectorAll(".comment-page")
+let closeThread = document.querySelectorAll(".close-thread")
 
 
 commentBtn.forEach((btn)=>{
-    btn.addEventListener("click", ()=>{
-        if(btn.parentElement.nextElementSibling.classList.contains("openComment")){
-            btn.parentElement.nextElementSibling.classList.remove("openComment")
-            // btn.parentElement.nextElementSibling.style.display = "none"
+    btn.addEventListener("click", (e)=>{
+        e.preventDefault()
+        if(btn.parentElement.nextElementSibling.style.display == "none"){
+            btn.parentElement.nextElementSibling.style.display = "flex"
         }else{
-            btn.parentElement.nextElementSibling.classList.add("openComment")
-            // btn.parentElement.nextElementSibling.style.display = "block"
+            btn.parentElement.nextElementSibling.style.display = "none"
         }
     })
 })
 
+
+closeThread.forEach((close)=>{
+    close.addEventListener("click", ()=>{
+        close.parentElement.parentElement.parentElement.parentElement.style.display = "none"
+    })
+})
+
 // open sub comments 
+let openSubComment = document.querySelectorAll(".openSubComment")
+let subCommentContent = document.querySelectorAll(".subComments")
+
+
+
+
+subCommentContent.forEach((comment)=>{
+    comment.style.display = "none"
+})
+
+openSubComment.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        if(btn.parentElement.parentElement.nextElementSibling.style.display == "none"){
+            btn.parentElement.parentElement.nextElementSibling.style.display = "block";
+        }else{
+            btn.parentElement.parentElement.nextElementSibling.style.display = "none";
+        }
+    })
+})
