03fcd3af91f6482a13c26cd46b07e2bee6ad1002
tommyjamess <meadigun@gmail.com>
2022-11-04 18:27:59 +0100

    update

--- styles/main.js
+++ styles/main.js
@@ -1,1 +1,21 @@
+let optionChoice = document.querySelectorAll(".optionChoice")
+let optionBtnInner = document.querySelectorAll(".option-btns-inner .selectOption")
+let submitOption = document.querySelectorAll(".optionForm button")
 
+optionChoice.forEach((item)=>{
+    item.style.display = "none";
+    
+})
+
+optionBtnInner.forEach((item)=>{
+    item.addEventListener("click", ()=>{
+        item.parentElement.previousElementSibling.style.display = "flex";
+    })
+})
+
+submitOption.forEach((item)=>{
+    item.addEventListener("click", (e)=>{
+        e.preventDefault()
+        item.parentElement.parentElement.parentElement.parentElement.style.display = "none";
+    })
+})
