74666dca30e4aa2a9d81847089be737c50198fc6
tommyjamess <meadigun@gmail.com>
2023-02-01 12:40:47 +0100

    update

--- styles/social_landing.js
+++ styles/social_landing.js
@@ -10,3 +10,52 @@ menuIcon.addEventListener("click", ()=> {
         linkList.style.maxHeight = "500px"
     }
 })
+
+
+let testimony = document.querySelectorAll(".testimony")
+let testimonyButton = document.querySelectorAll(".testimonyButton")
+
+testimony.forEach((item) => {
+    if(item.classList.contains("onDisplay")){
+        item.style.display = "flex"
+    }
+})
+
+testimonyButton.forEach((item, index) => {
+    if(index === 0){
+        item.style.background = "black"
+    }
+})
+
+testimonyButton.forEach((item, buttonIndex) => {
+    item.addEventListener("click", ()=> {
+        resetButtonColor()
+        displaySlideForButton(buttonIndex)
+        item.style.background = "black"
+
+    })
+})
+
+
+function resetButtonColor() {
+    testimonyButton.forEach((item) => {
+        item.style.background = "grey"
+    })
+}
+
+function displaySlideForButton(buttonIndex){
+    closeOtherDisplay()
+    testimony.forEach((item, displayIndex) => {
+        if(displayIndex == buttonIndex){
+            item.style.display = "flex"
+            item.style.transition = "5s"
+        }
+    })
+}
+
+function closeOtherDisplay(){
+    testimony.forEach((item) => {
+        item.style.display = "none"
+        item.style.transition = "5s"
+    })
+}
